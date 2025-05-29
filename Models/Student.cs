e0acdf8e3cd9a924f27f2161acdcf0b473d927dd
Muhammad Hamza <muhammadhamzasw517@gmail.com>
2025-05-29 22:13:00 +0500

    Created a student class

---
+++ Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CRUDApplication.Models
+{
+    public class Student
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; } = string.Empty;
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+
+        public string? Standard { get; set; }
+
+    }
+}
